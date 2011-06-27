625b5e8a4ccfe9d9cc3519ce6e77e91356118ebb
iiic <ic.czech@gmail.com>
2011-06-27 10:14:11 +0200

    first commit

---
+++ jquery.copywithq.js
@@ -0,0 +1,133 @@
+/**
+ * Copy With Q
+ *
+ * Automatic Quotes !
+ * Requires jQuery
+ * Q1 2011
+ *
+ * @copyleft	ic (icweb.eu)
+ * @license		CC BY (http://creativecommons.org/licenses/by/3.0/)
+ * @link			https://github.com/iiic/copywithq
+ * @version		0.1
+ * 
+ * Fork me on github @iiic
+ */
+
+var CopyWithQ = function(signature) {
+	this.signature = signature;
+};
+
+CopyWithQ.prototype = {
+
+	settings: // default values
+	{
+		'id': 'CopyWithQ-id',
+		'addLink': true,
+		'sourceLinkText': '',
+		'minLength': 80,
+		'preText': false
+	},
+
+	set: function(variables)
+	{
+		for(i in variables) {
+			this.settings[i] = variables[i];
+		}
+	},
+
+	newSelection: function()
+	{
+		if(window.getSelection) { // Chrome, Safari, Firefox
+			return this.windowGetSelection();
+		} else if (document.selection) { // IE
+			return this.documentSelection();
+		} // 'document.getSelection' for Opera
+		return false;
+	},
+
+	windowGetSelection: function()
+	{
+		var selection = window.getSelection();
+		if(!selection) {
+			return false;
+		}
+		if(!this.addQ(selection.toString())) {
+			return false;
+		}
+		var element = $('#'+this.settings['id']).get(-1);
+		var tempRange = document.createRange();
+		var range = selection.getRangeAt(0);
+		tempRange.selectNode(element);
+		selection.removeAllRanges();
+		selection.addRange(tempRange);
+		return function() {
+			$(element).remove();
+			selection.removeAllRanges();
+			selection.addRange(range)
+		}
+	},
+
+	documentSelection: function()
+	{
+		var range = document.selection.createRange();
+		var selectedHTML = range.htmlText;
+		if(!this.addQ(selectedHTML)) {
+			return false;
+		}
+		var element = $('#'+this.settings['id']).get(-1);
+		var textRange = document.body.createTextRange();
+		textRange.moveToElementText(element);
+		textRange.select();
+		return function () {
+			$(element).remove();
+			range.select();
+		}
+	},
+
+	addQ: function(selectedHTML)
+	{
+		if(selectedHTML.length < this.settings['minLength']) {
+			return false;
+		}
+		if(this.settings['preText']) {
+			selectedHTML = '<pre>'+selectedHTML+'</pre>';
+		}
+		if(this.signature) {
+			if(this.settings['addLink']) {
+				selectedHTML += '<blockquote> -- '+this.signature+'<br>'+this.settings['sourceLinkText']+'<a href="'+location.href+'">'+location.href+'</a></blockquote>';
+			} else {
+				selectedHTML += '<blockquote> -- '+this.signature+'</blockquote>';
+			}
+		} else {
+			if(this.settings['addLink']) {
+				selectedHTML += '<blockquote style="font-style:italic">'+this.settings['sourceLinkText']+'<a href="'+location.href+'">'+location.href+'</a></blockquote>';
+			} else {
+				return true;
+			}
+		}
+		$('<div>', {
+			id: this.settings['id'],
+			style: 'position:absolute;top:-9px;width:0;height:0;overflow:hidden'
+		}).appendTo('body');
+		$('#'+this.settings['id']).append(selectedHTML);
+		return true;
+	},
+
+	catchSelection: function(signature)
+	{
+		this.signature = signature;
+		var end = this.newSelection();
+		if(end) {
+			setTimeout(end, 1);
+		}
+	},
+
+	run: function()
+	{
+		sig = this.signature;
+		$('body').bind('copy', function() { // NOT working with Opera 11.11
+			CopyWithQ.prototype.catchSelection(sig);
+		});
+	}
+
+};
---
+++ readme.md
@@ -0,0 +1,74 @@
+  CopyWithQ
+=============
+
+** Automatické citace **
+
+*
+Jednoduchý javascript pro automatickou tvorbu citací. Uživateli stačí oznatit
+část textu, běžným způsobem ji zkopírovat (funguje jak zkratka Ctrl + c,
+tak použití kontextové nabýdky vyvolané pravým talčítkem myši),a citace se do
+kopírovaného textu doplní automaticky.
+*
+
+
+
+Použití
+-------
+
+Soubor se scriptem vložte kamkoliv do stránky, jako jakýkoliv běžný javascript.
+Nezapomeňte samozřejmě i na jQuery.
+Při vytváření instance třídy můžete konstruktoru předat paramatr (řetězec),
+který následně bude použit jako autor kopírovaného textu.
+javascript:
+
+	var linkQuote = new CopyWithQ('Jon Doe');
+
+následně metoda set může upravit chování scriptu:
+
+	linkQuote.set({proměnná:hodnota, proměnná:hodnota, ...});
+
+nastavení obsahuje následující volby
+
+* `id`: řetězec identifikátor elementu, který se musí při běhu scriptu vytvořit,
+* `addLink`: pokud se nastaví na false nepřidá do citace odkaz na původní web,
+* `sourceLinkText`: řetězec... vloží se před odkaz směřující na zdrojový web,
+* `minLengthv`: integer, který určuje od kolika vybraných znaků se přidá citace,
+* `preText`: bool, který při true zachová původní formátování zvoleného textu
+
+Kolize id s jiným elementem s totožným id by neměla způsobit selhání sctiptu.
+Objekt se vytváří na konci těla stránky (body) a z případných stejných id
+se pracuje s poslením z nich.
+
+Nakonec se script spustí metodou run:
+
+	linkQuote.run();
+
+### jednoduchý příklad použití je v souboru `example.html` ###
+
+
+
+Testováno s pomocí jQuery 1.6.1 na prohlížečích
+-----------------------------------------------
+
+* funguje: *
+* IE 8.0.6001.18702 (nepříjemné probliknutí na stránkách s posuvníkem)
+* FF 5.0
+* Chrome 12.0.742.91
+* Chrome 14.0.786.0 (canary build)
+* Safari 5.0.2
+
+* nefunguje: *
+* Opera 11.11
+
+
+
+Licence
+-------
+
+Celý sctipt je vydán pod licencí Creative Commons verze 3.0 BY
+[CC BY] (http://creativecommons.org/licenses/by/3.0/cz/)
+
+Budu rád, když v souladu s licencí zachováte úvodní víceřádkový komentář
+v javascript souboru ( `jquery.copywithq.js` )
+
+					ic
